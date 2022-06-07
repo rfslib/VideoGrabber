17cc2ac68d51427d4187a08b9a7ed09f7fbd3e7c
Ed C <edcii@ignap.com>
2022-06-07 17:54:03 -0600

    show filename, get length from obs

--- videograbber.py
+++ videograbber.py
@@ -39,10 +39,11 @@ expected_obs_version = '27.2.4'
 expected_ws_version = '4.9.1'
 expected_simpleobsws_version = '1.1'
 
-vr_title = 'RFSL Video Recorder'
-vr_geometry = '450x250+48+48'
+vr_title = 'Riverton FamilySearch Library Video Grabber'
+vr_geometry = '500x180+48+48'
 
-default_bg = 'SystemButtonFace'
+bg_color = 'SystemButtonFace'
+bg_alpha = 0.95
 
 obs_command = r'C:\Program Files\obs-studio\bin\64bit\obs64.exe'
 obs_directory = r'C:\Program Files\obs-studio\bin\64bit'
@@ -55,12 +56,14 @@ btn_width = 8
 st_font = 'Lucida Console'
 st_font_size = 16
 
+
+recording_filename_font = 'Lucida Console'
+recording_filename_fontsize = 9
+
 recording_in_progress = False
 elapsed_time = 0
 elapsed_time_font = 'Lucida Console'
 elapsed_time_fontsize = 9
-
-recording_filename = ''
 
 free_disk = 0.0
 free_disk_min = 5000.0
@@ -104,18 +107,17 @@ def show_disk_space( ):
     if free_disk < free_disk_min:
         ds.config( bg = 'Red' )
     else:
-        ds.config( bg = default_bg )
+        ds.config( bg = bg_color )
     disk_space_text.set( f'Available disk space: {free_disk/1024:.1f}G ' )
     vr.after( fd_delay, show_disk_space )
 
 async def __start_recording( ):
-    global recording_filename
     await ws.connect()
     rc = await ws.call( 'StartRecording' )   
     if debug: print( f'start_recording rc: {rc}')
     await asyncio.sleep( 1 )
-    info = await ws.call( 'GetRecordingStatus')
-    recording_filename = basename( info[ 'recordingFilename' ] )
+    info = await( ws.call( 'GetRecordingStatus' ) )
+    recording_filename.set( basename( info[ 'recordingFilename' ] ) )
     await ws.disconnect( )
 
 def start_recording( ):
@@ -125,25 +127,31 @@ def start_recording( ):
     recording_in_progress = True
     loopy.run_until_complete( __start_recording( ) )
     show_recording_status( 'Recording', 'Red' )
-    elapsed_time = 1
+    elapsed_time = 0
     show_elapsed_time()
     btn_stop[ 'state' ] = NORMAL
 
 async def __stop_recording( ):
     await ws.connect()
+    # TODO: use GetRecordingStatus to get the length of the recording
+    info = await( ws.call( 'GetRecordingStatus' ) )
+    await asyncio.sleep( 1 )
+    if debug: print( f'GetRecordingStatus.recordTimecode {info[ "recordTimecode" ]}')
+    recording_time.set( info[ 'recordTimecode' ] )
     rc = await ws.call( 'StopRecording' )    
     if debug: print( f'stop_recording rc: {rc}')
-    await asyncio.sleep( 1 )
+    
     await ws.disconnect( )
 
 def stop_recording( ):
-    global recording_in_progress, recording_filename
-    btn_stop[ 'state' ] = DISABLED
-    loopy.run_until_complete( __stop_recording( ) )
+    global recording_in_progress
     recording_in_progress = False
+    btn_stop[ 'state' ] = DISABLED
+    loopy.run_until_complete( __stop_recording( ) )    
     show_recording_status( 'Stopped', 'Black' )
-    show_app_status( f'Saved to {recording_filename} on the desktop', 'Black' )
+    show_app_status( f'File "{recording_filename.get()}" saved to the desktop', 'Grey' )
     btn_start[ 'state' ] = NORMAL
+
 
 def show_recording_status( txt, color ): # Show status message next to buttons
     recording_state_label.config( fg = color )
@@ -156,21 +164,24 @@ def show_app_status( txt, color ):
 def show_elapsed_time():
     global recording_in_progress, elapsed_time
     if debug: print( f'show_elapsed_time: {elapsed_time}')
-    recording_time.set( str( timedelta( seconds=elapsed_time ) ) )
     if recording_in_progress:
+        recording_time.set( str( timedelta( seconds=elapsed_time ) ) )
         elapsed_time += 1
         vr.after( 1000, show_elapsed_time )
+    #else:
+        #recording_time.set( '' )
 
 
 def is_process_running( processName ): # https://thispointer.com/python-check-if-a-process-is-running-by-name-and-find-its-process-id-pid/
-    '''
-    Check if there is any running process that contains the given name processName.
-    '''
+    ''' Check if there is any running process that contains the given name processName. '''
     for proc in psutil.process_iter(): #Iterate over the all the running processes
         try:
             if processName.lower() in proc.name().lower(): # Check if process name contains the given name string
                 return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            return False
+        except (psutil.ZombieProcess):
+            # TODO: need to kill it
             return False
 
 def start_obs( ):
@@ -181,21 +192,37 @@ def start_obs( ):
         btn_start[ 'state' ] = DISABLED
         btn_stop[ 'state' ] = DISABLED
         show_app_status( 'ERROR: OBS could not be started', 'Red' )
-        if debug: print( 'ERROR: OBS could not be started' )    
+        if debug: print( 'ERROR: OBS could not be started' ) 
+
+async def __configure_obs( ):
+    await ws.connect()
+    #rc = await ws.call( 'SetFilenameFormatting' ) 
+    #if debug: print( f'SetFilenameFormatting: {rc}') 
+    # TODO: GetSourcesList, CreateSource, SetVolume, SetSourceSettings, SetCurrentProfile, ListProfiles, GetRecordingStatus, SetRecordingFolder, SetCurrentScene, CreateScene,  
+    await asyncio.sleep( 1 )
+    await ws.disconnect( )
+    pass
+
+def configure_obs( ):
+    loopy.run_until_complete( __configure_obs( ) )
+    pass
+
+def log_callback( ): 
+    pass  
 
 #------
 if __name__ == '__main__':
     vr = Tk()
+    vr.attributes( '-alpha', bg_alpha ) # set transparency
+    vr.attributes( '-topmost', 1 ) # force it to stay on top (so user doesn't lose it)
     vr.geometry( vr_geometry )
     vr.resizable( False, False )
     vr.title( vr_title )
-    vr.iconbitmap( 'VideoRec.ico' )
-    #vr.config( bg='LightBlue' )
-    vr.attributes( '-topmost', 1 ) # force it to stay on top (so user doesn't lose it)
+    vr.iconbitmap( 'VideoRec.ico' )  
 
     # frame for start button
     fr1 = Frame( master=vr, padx = 8, pady = 8 )
-    fr1.grid( row=0, column=0, rowspan=2, padx = 4, pady = 4, sticky='wn' )
+    fr1.grid( row=0, column=0, rowspan=3, padx = 4, pady = 4, sticky='wn' )
 
     # create 'start' button
     btn_start = Button( fr1, text = 'Record',
@@ -208,7 +235,7 @@ if __name__ == '__main__':
 
      # frame for stop
     fr2 = Frame( master=vr, padx = 8, pady = 8 )
-    fr2.grid( row=0, column=1, rowspan=2, padx = 4, pady = 4, sticky='wn' )
+    fr2.grid( row=0, column=1, rowspan=3, padx = 4, pady = 4, sticky='wn' )
    
     # create 'stop' button
     btn_stop = Button( fr2, text = 'Stop',
@@ -219,23 +246,30 @@ if __name__ == '__main__':
         )
     btn_stop.grid( row=0, column=0 )
 
-    # frame/label for recording state
+    # frame/label for recording state, filename, time
     recording_state_frame = Frame( master=vr, padx=8, pady=8 )
     recording_state_frame.grid( row=0, column=2, padx=4, pady=4, sticky='wn')
     recording_state = StringVar()
     recording_state_label = Label( recording_state_frame, textvariable=recording_state, font=( st_font, st_font_size ), anchor='w' )
     recording_state_label.grid( sticky='w' )
 
+    # frame/label for output filename
+    #recording_filename_frame = Frame( master=vr, padx=8, pady=8 )
+    #recording_filename_frame.grid( row=1, column=2, padx=4, pady=4, sticky='w')
+    recording_filename = StringVar()
+    recording_filename_label = Label( recording_state_frame, textvariable=recording_filename, font=( recording_filename_font, recording_filename_fontsize ), anchor='w' )
+    recording_filename_label.grid( sticky='w')
+    
     # frame/label for recording time
-    recording_time_frame = Frame( master=vr, padx=8, pady=8 )
-    recording_time_frame.grid( row=1, column=2, padx=4, pady=4, sticky='ws' )
+    #recording_time_frame = Frame( master=vr, padx=8, pady=8 )
+    #recording_time_frame.grid( row=2, column=2, padx=4, pady=4, sticky='ws' )
     recording_time = StringVar()
     recording_time_label = Label( recording_state_frame, textvariable=recording_time, font=( elapsed_time_font, elapsed_time_fontsize ), anchor='w' )
     recording_time_label.grid( sticky='w' )
 
     # frame/label for disk space
     fr3 = Frame( master=vr, padx = 8, pady = 8 )
-    fr3.grid( row = 2, column=0, columnspan=3, sticky='w' )
+    fr3.grid( row = 3, column=0, columnspan=3, sticky='w' )
     disk_space_text = StringVar()
     ds = Label( fr3, textvariable=disk_space_text, font=( fd_font, fd_font_size ), anchor='w' )
     ds.grid( sticky='w')
@@ -243,10 +277,12 @@ if __name__ == '__main__':
 
     # frame/label for app status
     app_status_frame = Frame( master=vr, padx = 8, pady = 8 )
-    app_status_frame.grid( row = 3, column=0, columnspan=3, sticky='w' )
+    app_status_frame.grid( row = 4, column=0, columnspan=3, sticky='w' )
     app_status_text = StringVar()
     app_status = Label( fr3, textvariable=app_status_text, font=( app_status_font, app_status_font_size ), anchor='w' )
     app_status.grid( sticky='w' )
+
+    vr.update()
 
     # if OBS isn't running, start it
     if not is_process_running( 'obs64.exe' ):
@@ -259,6 +295,8 @@ if __name__ == '__main__':
     loopy.run_until_complete( get_obs_info( ) )
     
     show_app_status( f'obs: {obs_version}, ws: {ws_version}, status: {obs_status}', 'Grey')
+
+    vr.update()
 
     show_disk_space() # schedules itself to re-run every fd_delay milliseconds (default one minute)
 
